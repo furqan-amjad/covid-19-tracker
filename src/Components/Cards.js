66965bf10021e56618a737bee62b75dde7b64f1f
furqan-amjad <furqanamjad13@gmail.com>
2021-05-21 00:30:53 +0500

    update

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-//import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Paper from './Paper';
-import {useEffect, useState} from 'react';
+import Header from './Header';
+import paper from '@material-ui/core/Paper';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,48 +21,20 @@ const useStyles = makeStyles((theme) => ({
 export default function Cards() {
   const classes = useStyles();
 
-  const [apiData, setApiData] = useState();
-
-  useEffect( () => {
-    async function fetchData(){
-      const url = "https://covid19.mathdro.id/api";
-      const url1 = "https://randomuser.me/api/";
-      const apiResponse = await fetch(url1);
-      //console.log(apiResponse);
-      const apiResponseData = await apiResponse.json();
-      console.log(apiResponseData);
-      setApiData(apiResponseData);
-    }
-    fetchData();
-  },[])
-
 
   return (
     <div className={classes.root}>
+      <Header />
       <Grid container spacing={3}>
 
         <Grid item xs={2} sm={3}>
-          Transparent Grid
         </Grid>
 
         <Grid item xs={8} sm={6}>
-          <Grid container spacing={3}>
-            <Grid item xs={4} sm={4}>
-              <Paper>
-                {apiData.results.Array.email}                
-              </Paper>
-            </Grid>
-            <Grid item xs={4} sm={4}>
-              <Paper />
-            </Grid>
-            <Grid item xs={4} sm={4}>
-              <Paper />
-            </Grid>
-          </Grid>
+          <Paper/>
         </Grid>
 
         <Grid item xs={2} sm={3}>
-          Transparent Grid
         </Grid>
 
       </Grid>
--- src/Components/Paper.js
+++ src/Components/Paper.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,12 +18,91 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const useStylesTypography = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
+
+const useStylesPaper = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 export default function SimplePaper() {
   const classes = useStyles();
+  const classes1 = useStylesTypography();
+  const paperClass = useStylesPaper();
+
+ 
+    const [apiData, setApiData] = useState();
+  
+    useEffect( () => {
+      async function fetchData(){
+        const url = "https://covid19.mathdro.id/api";
+        //const {apiResponse} = await fetch(url);
+        const response = await axios.get(url);
+        setApiData(response);
+        console.log(response);
+        console.log(apiData);
+        //console.log(response.data.confirmed.value)
+  
+      }
+      fetchData();
+    },[])
+
+
+
 
   return (
-    <div className={classes.root}>
-      <Paper elevation={3} />
+    <div className={classes.root, classes1.root, paperClass.root}>
+      <Grid container spacing={3}>
+            <Grid item xs={4} sm={4} className={classes.root}>
+              <Paper className={paperClass.paper} elevation={3}>
+           
+                <Typography variant="h4" gutterBottom className={classes1.root} style={{color: "blue"}}>
+                  {apiData && apiData.data && apiData.data.confirmed && apiData.data.confirmed.value}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom className={classes1.root} style={{color: "blue"}}>
+                  Confirmed Cases
+                </Typography>
+
+              </Paper>
+              
+            </Grid>
+            <Grid item xs={4} sm={4} className={classes.root}>
+              <Paper className={paperClass.paper} elevation={3} >
+              
+                <Typography variant="h4" gutterBottom className={classes1.root} style={{color: "red"}}>
+                  {apiData && apiData.data && apiData.data.deaths && apiData.data.deaths.value}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom className={classes1.root} style={{color: "red"}}>
+                  Deaths
+                </Typography>
+              
+              </Paper>
+            </Grid>
+            <Grid item xs={4} sm={4} className={classes.root}>
+              <Paper className={paperClass.paper} elevation={3}>
+     
+                <Typography variant="h4" gutterBottom className={classes1.root} style={{color: "green"}}>
+                  {apiData && apiData.data && apiData.data.recovered && apiData.data.recovered.value}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom className={classes1.root} style={{color: "green"}}>
+                  Recovered
+                </Typography>
+
+              </Paper>
+
+            </Grid>
+      </Grid>
     </div>
   );
 }
